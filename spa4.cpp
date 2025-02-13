73a8d0e6a40b5af6d22f197138f0ca4dcfec43e0
Haniyehjb <haniyehjb1883@gmail.com>
2025-02-13 20:29:34 +0330

    reducing the number of lines with defines

---
+++ spa4.cpp
@@ -0,0 +1,21 @@
+#include<iostream> #define b-1 bdddbb #define b+2 bbddbd
+#define bbbdbd char
+#define dbdbdd goto
+#define a bbddbb
+#define x dbbdbb
+int main(){ int a=753,dbd,bd,dbddbd,dbbdbd,l1l1=37,l111=67,lll1=14,
+    bdbddb=2,ll11=1,l1ll=61,l11l=19,llll=44;long long c=0x7368656c646f6e;
+    bbbdbd*dbddbb=(bbbdbd*)&c-((bbddbb&0xF)*(0xad&0xf>>8<<((5^5)&(~(
+    1<<3)|(7&7)))))+bdbddb;bbbdbd dbbdbb[]={(*(--dbddbb)-bbddbb%l111
+    ),(*(dbddbb-1)+bbddbb%lll1),(*(dbddbb+bdbddb)-bbddbb%l1l1),(*(++
+    dbddbb)++-bbddbb%l1ll),(*((++++dbddbb)--)+bbddbb%l11l),(*(dbddbb
+    )-bbddbb%llll)};void*labels[]={&&l11lll1ll,&&end,&&ll1lll1l1,&&
+    l11lll1l1};dbdbdd*labels[0b0011];ll1lll1l1:std::cout<<dbbdbb[bd]<<x
+    [dbddbd]<<dbbdbb[dbd];dbdbdd*labels[((('A'-'B'+3)*('C'^'D')%'E'!='F'-
+    'G'))];l11lll1l1:dbbdbd=bd=((*(dbddbb+2)|~(0xFF89))^(sizeof(x+c)
+    <<3))%6;dbddbd=((dbbdbd[dbbdbb])&((bbbdbd)(c+4)+dbbdbd*5)|0xFF)%6
+    ;dbdbdd*labels[(0xF&0b1010^0xA)];l11lll1ll:dbd=(dbbdbd+dbddbd>6)?
+    (dbbdbd==5?((dbbdbd--)[dbbdbb]^(*(--dbddbb)+bbddbb%dbddbd))% 6:(
+    (dbbdbd++)[dbbdbb]|((bbbdbd)c+lll1%dbddbd)%6)):(((bbbdbd)c+dbbdbd
+    *dbddbd)&(*(dbddbb++)+bd%dbddbd))%6;dbdbdd*labels[(0b10 &0x2)];end:
+    return 0;}
