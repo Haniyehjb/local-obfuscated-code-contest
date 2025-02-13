8959ba0f39f6039e6229016ffe7af2d00cfdece5
Haniyehjb <haniyehjb1883@gmail.com>
2025-02-13 21:54:25 +0330

    implementing a dialog from big bug theory

---
+++ test1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#define bbbdbd char
+#define dbdbdd goto
+#define bbddbb a
+#define I int a=753,dbd,bd,dbddbd,dbbdbd , l1l1=37 ,l111=67 ,lll1=14 ,\
+bdbddb=2 ,ll11=1 ,l1ll=61 ,l11l=19 ,llll = 44;long long dd=0x7368656c646f6e;
+#define Am bbbdbd*dbddbb=(bbbdbd*)&dd-((bbddbb&0xF)*(0xad&0xf>>8<<((5^5)&(~\
+(1<<3)|(7&7)))))+bdbddb;bbbdbd dbbdbb[]={(*(--dbddbb)-bbddbb%l111),(*(
+#define Not dbddbb-1)+bbddbb%lll1),(*(dbddbb+bdbddb)-bbddbb%l1l1),(*(++\
+dbddbb)++-bbddbb%l1ll),(*((++++dbddbb)--)+bbddbb%l11l),(*(dbddbb)-bbddbb
+#define CRAZY %llll)};void*ll1[]={&&l11lll1ll,&&end,&&ll1lll1l1,&&\
+l11lll1l1};dbdbdd*ll1[0b0011];ll1lll1l1:std::cout<<dbbdbb[bd]<<dbbdbb[
+#define My dbddbd]<<dbbdbb[dbd];dbdbdd*ll1[1];l11lll1l1:dbbdbd=bd=((*(\
+dbddbb+2)|~(0xFF89))^(sizeof(dbbdbb)<<3))%6;dbddbd=((dbbdbd[dbbdbb])&((
+#define Mother bbbdbd)(dd+4)+dbbdbd*5)|0xFF)%6;dbdbdd*ll1[(0xF&0b1010^0xA)];
+#define Had l11lll1ll:dbd=(dbbdbd+dbddbd>6)?(dbbdbd==5?((dbbdbd--)[dbbdbb]^(*
+#define Me (--dbddbb)+bbddbb%dbddbd))% 6:((dbbdbd++)[dbbdbb]|((bbbdbd)dd+\
+lll1%dbddbd)%6)):(((bbbdbd)dd+dbbdbd*dbddbd)&(*(dbddbb++)+bd%dbddbd))%6;
+#define Tested dbdbdd*ll1[(0b10 &0x2)];end:return 0;
+int main(){I Am Not CRAZY My Mother Had Me Tested}
