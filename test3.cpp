30955e0579ca4198f737e797e9320126390a9564
Haniyehjb <haniyehjb1883@gmail.com>
2025-02-13 22:46:36 +0330

    final result with 21 lines

---
+++ test3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#define dbdbdd goto
+#define bbddbb a
+#define I int a=753,dbd,bd,dbddbd,dbbdbd , l1l1=37 ,l111=67 ,lll1=14 ,\
+bdbddb=2 ,ll11=1 ,l1ll=61 ,l11l=19 ,llll = 44;long long dd=0x7368656c646f6e;
+#define Am   char*dbddbb                       =(char*)&dd-((bbddbb&0xF)*(0xad&0xf>>8 \
+<<((5^5)&(~(1<<3)|(7&7))                       )))+bdbddb;char dbbdbb[]={(*(--dbddbb)
+#define Not -bbddbb%l111          ),(*(dbddbb - 1)+bbddbb%lll1),(*(dbddbb+ \
+bdbddb)-bbddbb%l1l1),(*(          ++dbddbb)++-bbddbb%l1ll),(*((++++dbddbb)--)
+#define CRAZY +   bbddbb          %l11l),(*(dbddbb)-bbddbb%llll)};void*ll1[]= \
+{&&l11lll1ll,      &&end                      ,&&ll1lll1l1,&&l11lll1l1};
+#define My        dbdbdd                      *ll1[0b0011];ll1lll1l1:std::cout \
+<<dbbdbb[bd]<<    dbbdbb          [dbddbd]<<dbbdbb[dbd];dbdbdd*ll1[1];         \
+l11lll1l1:dbbdbd=bd=((*(          dbddbb+2)|~(0xFF89))^(sizeof(dbbdbb)<<3))%6;
+#define Mother dbddbd=((          dbbdbd[dbbdbb])&((char)(dd+4)+dbbdbd*5)
+#define Had |0xFF)   %6;                      dbdbdd*ll1[(0xF&0b1010^0xA)];
+#define Me l11lll1ll:dbd                      =(dbbdbd+dbddbd>6)?(dbbdbd==5? \
+((dbbdbd--)[dbbdbb]^(*(--dbddbb)+bbddbb%dbddbd))% 6:((dbbdbd++)[dbbdbb]|
+#define Tested ((char)dd+lll1%dbddbd)%6)):(((char)dd+dbbdbd*dbddbd)& \
+(*(dbddbb++)+bd%dbddbd))%6;dbdbdd*ll1[(0b10 &0x2)];end:return 0;
+int main(){I Am Not CRAZY My Mother Had Me Tested}
