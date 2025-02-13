a2405d76dde9391af9979a817db95cc5935a0338
Haniyehjb <haniyehjb1883@gmail.com>
2025-02-13 11:42:31 +0330

    define numbers

---
+++ nums.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+#define b-1 bdddbb
+#define b+2 bbddbd
+#define l1l1 37
+#define l111 67
+#define lll1 14
+#define bdbddb 2
+#define ll11 1
+#define l1ll 61
+#define l11l 19
+#define l11l 44
+#define o0o0o char
+#define b dbddbb
+int main(){
+    int a=753,d,o,n,m;
+    long long c=0x7368656c646f6e;
+    o0o0o*b=(o0o0o*)&c-((a & 0xF)*(0xad&0xf>>8<<((5^5)&(~(1<<3)|(7&7)))))+2;
+    o0o0o x[] ={(*(--dbddbb)-a%l111),(*(dbddbb-1)+a%lll1),(*(dbddbb+bdbddb)-a%l1l1),(*(++dbddbb)++-a%l1ll),(*((++++dbddbb)--)+a%l11l),(*(dbddbb)-a%l11l)};
+    void*labels[]={&&l11lll1ll,&&end,&&ll1lll1l1,&&l11lll1l1};
+    goto*labels[0b0011];
+    ll1lll1l1:
+    std::cout<<x[o]<<x[n]<<x[d];
+    goto*labels[((('A'-'B'+3)*('C'^'D')%'E'!='F'-'G'))];
+    l11lll1l1:
+    m=o=((*(b+2)|~(0xFF89))^(sizeof(x+c)<<3))%6;
+    n=((m[x])&((o0o0o)(c+4)+m*5)|0xFF)%6;
+    goto*labels[(0xF&0b1010^0xA)];
+    l11lll1ll:
+    d=(m+n>6)?
+      (m==5?((m--)[x]^(*(--b)+a%n))% 6:((m++)[x]|((o0o0o)c+14%n)%6))
+             :(((o0o0o)c+m*n)&(*(b++)+o%n))%6;
+    goto*labels[(0b10 &0x2)];
+    end:
+    return 0;
+}
