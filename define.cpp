2a0e177181b56d12aad4dea31644c59679bcbb67
Haniyehjb <haniyehjb1883@gmail.com>
2025-02-13 12:33:37 +0330

    all defines

---
+++ define.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+#define b-1 bdddbb
+#define b+2 bbddbd
+#define l1l1 37
+#define l111 67
+#define lll1 14
+#define bdbddb 2
+#define ll11 1
+#define l1ll 61
+#define l11l 19
+#define l11l 44
+#define bbbdbd char
+#define b dbddbb
+#define a bbddbb
+#define n dbddbd
+#define m dbbdbd
+#define x dbbdbb
+
+int main(){
+    int a=753,d,o,n,m;
+    long long c=0x7368656c646f6e;
+    bbbdbd*dbddbb=(bbbdbd*)&c-((bbddbb&0xF)*(0xad&0xf>>8<<((5^5)&(~(1<<3)|(7&7)))))+bdbddb;
+    bbbdbd dbbdbb[] ={(*(--dbddbb)-bbddbb%l111),(*(dbddbb-1)+bbddbb%lll1),(*(dbddbb+bdbddb)-bbddbb%l1l1),(*(++dbddbb)++-bbddbb%l1ll),(*((++++dbddbb)--)+bbddbb%l11l),(*(dbddbb)-bbddbb%l11l)};
+    void*labels[]={&&l11lll1ll,&&end,&&ll1lll1l1,&&l11lll1l1};
+    goto*labels[0b0011];
+    ll1lll1l1:
+    std::cout<<dbbdbb[o]<<x[dbddbd]<<dbbdbb[d];
+    goto*labels[((('A'-'B'+3)*('C'^'D')%'E'!='F'-'G'))];
+    l11lll1l1:
+    dbbdbd=o=((*(dbddbb+2)|~(0xFF89))^(sizeof(x+c)<<3))%6;
+    n=((dbbdbd[dbbdbb])&((bbbdbd)(c+4)+dbbdbd*5)|0xFF)%6;
+    goto*labels[(0xF&0b1010^0xA)];
+    l11lll1ll:
+    d=(dbbdbd+dbddbd>6)?
+      (dbbdbd==5?((dbbdbd--)[dbbdbb]^(*(--dbddbb)+bbddbb%dbddbd))% 6:((dbbdbd++)[dbbdbb]|((bbbdbd)c+lll1%dbddbd)%6))
+             :(((bbbdbd)c+dbbdbd*dbddbd)&(*(dbddbb++)+o%dbddbd))%6;
+    goto*labels[(0b10 &0x2)];
+    end:
+    return 0;
+}
